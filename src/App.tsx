a970983a949b986bff716b2faf691a9df8d30f9c
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-31 11:48:05 -0500

    set up, start props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+interface Istate {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    // question mark indicates it is optional
+    note?: string
+  }[]
+}
+
 function App() {
+
+
+const [people, setPeople] = useState<Istate["people"]>([]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to our Party!</h1>
     </div>
   );
 }
